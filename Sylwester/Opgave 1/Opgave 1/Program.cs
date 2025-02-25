29a998de46c14665426d4401e2551e0c79ce5c75
Sylwester Karbowski <163668119+s-karbowski@users.noreply.github.com>
2025-02-25 14:06:21 +0100

    Add files via upload

---
+++ Sylwester/Opgave 1/Opgave 1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Opgave_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                string name = "Alice";
+
+                int age = 25;
+
+                char initial = 'A';
+
+                double height = 1.75;
+
+                float weight = 70.5f;
+
+                bool isStudent = true;
+
+
+                Console.WriteLine($"Age: {age}, Height: {height}, Weight: {weight}, Initial: {initial}, Name: {name}, Is Student: {isStudent}");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
