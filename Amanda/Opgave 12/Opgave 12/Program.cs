1a7bd1bc70d89ceab53b6e62c7c3c43945844cdd
Amanda <amandajuhlsilberg@hotmail.com>
2025-02-25 14:34:40 +0100

    Tilføjede opgave 12

---
+++ Amanda/Opgave 12/Opgave 12/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Opgave_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Opgave 12
+            Console.WriteLine("Nu skriver vi tal fra 1 til 10");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
