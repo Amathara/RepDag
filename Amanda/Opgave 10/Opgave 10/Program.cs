e68eed3a7838f75afdb62f854fe670270c49345b
Amanda <amandajuhlsilberg@hotmail.com>
2025-02-25 14:32:26 +0100

    tilføjede opgave 10

---
+++ Amanda/Opgave 10/Opgave 10/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Opgave_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Opgave 10
+            // Skriv en ugedag
+ Console.WriteLine("Hej!\nLad os finde en ugedag\nSkriv et tal mellem 1 og 7");
+ int dag = Convert.ToInt32(Console.ReadLine());
+
+ switch(dag)
+ {
+     case 1:
+         Console.WriteLine("Det er Mandag!");
+         break;
+         case 2:
+         Console.WriteLine("Det er Tirsdag!");
+         break;
+         case 3:
+         Console.WriteLine("Det er Onsdag!");
+         break;
+         case 4:
+         Console.WriteLine("Det er Torsdag!");
+         break;
+         case 5:
+         Console.WriteLine("Det er Fredag! (så det snart weekend)");
+         break;
+         case 6:
+         Console.WriteLine("Det er Lørdag! Enjoy!");
+         break;
+         case 7:
+         Console.WriteLine("Det er Søndag!");
+         break;
+
+
+
+ }
+ Console.ReadLine();
+}
+}
+}
