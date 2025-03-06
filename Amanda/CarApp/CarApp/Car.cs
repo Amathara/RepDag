3f46298028b0a3131449a4ee4efd396c26307571
Amanda <amandajuhlsilberg@hotmail.com>
2025-03-06 10:41:08 +0100

    Added Constuctor and Method to Car.cs

--- Amanda/CarApp/CarApp/Car.cs
+++ Amanda/CarApp/CarApp/Car.cs
@@ -8,5 +8,32 @@ namespace CarApp
 {
     internal class Car
     {
+        string _brand;
+        string _model;
+        int _year;
+        char _gear;
+        double _odometer;
+        string _fuelType;
+        bool _isEngineOn;
+        double _kmPerLiter;
+
+        //Constructor:
+        public Car(string brand,string model, int year, char gear, double odometer, string fuelType, bool isEngineOn, double kmPerLiter)
+        {
+            _brand = brand;
+            _model = model;
+            _year = year;
+            _gear = gear;
+            _odometer = odometer;
+            _fuelType = fuelType;
+            _isEngineOn = isEngineOn;
+            _kmPerLiter = kmPerLiter;
+                
+        }
+        // Method: Get car info.
+        public string GetCarInfo()
+        {
+            return $"Brand: {_brand}\n\nModel: {_model}\n\nYear: {_year}\n\nGear: {_gear}\n\nMilage: {_odometer}\n\nFuel Type: {_fuelType}\n\n Engine is on: {_isEngineOn};\n\n Can drive: {_kmPerLiter} km per liter of fuel";
+        }
     }
 }
